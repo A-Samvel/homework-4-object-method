cd0954dad290944265c0ac0208dd98538ce57385
Samvel Antonyan <samvelantonyan1998@gmail.com>
2024-09-03 16:33:56 +0400

    add fumction lowestScoreName

---
+++ object-homework/lowestScoreName.js
@@ -0,0 +1,86 @@
+const evaluations = [
+  {
+    studentId: 1,
+    studentName: "John Doe",
+    courseId: 1,
+    courseName: "JS Bootcamp",
+    score: 70,
+  },
+
+  {
+    studentId: 1,
+    studentName: "John Doe",
+    courseId: 2,
+    courseName: "JS Level 0",
+    score: 60,
+  },
+
+  {
+    studentId: 1,
+    studentName: "John Doe",
+    courseId: 3,
+    courseName: "JS Level 1",
+    score: 50,
+  },
+
+  {
+    studentId: 2,
+    studentName: "Tom Colins",
+    courseId: 1,
+    courseName: "JS Bootcamp",
+    score: 60,
+  },
+
+  {
+    studentId: 2,
+    studentName: "Tom Colins",
+    courseId: 2,
+    courseName: "JS Level 0",
+    score: 50,
+  },
+
+  {
+    studentId: 2,
+    studentName: "Tom Colins",
+    courseId: 3,
+    courseName: "JS Level 1",
+    score: 40,
+  },
+
+  {
+    studentId: 3,
+    studentName: "Sarah Conor",
+    courseId: 1,
+    courseName: "JS Bootcamp",
+    score: 45,
+  },
+
+  {
+    studentId: 3,
+    studentName: "Sarah Conor",
+    courseId: 2,
+    courseName: "JS Level 0",
+    score: 35,
+  },
+
+  {
+    studentId: 3,
+    studentName: "Sarah Conor",
+    courseId: 3,
+    courseName: "JS Level 1",
+    score: 25,
+  },
+];
+
+function lowestScoreName(arr = []) {
+  const name = arr.reduce(
+    (acc, value) => {
+      if (value["score"] < acc["score"]) return value;
+      else return acc;
+    },
+    { score: Infinity }
+  );
+  return name.studentName;
+}
+
+console.log(lowestScoreName(evaluations));
